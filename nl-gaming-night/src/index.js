c7265e2022a997a017eeb8f1c01d5b732f146670
Jason Chung <jasonckc07@gmail.com>
2018-09-09 20:52:19 -0400

    Added React

---
+++ nl-gaming-night/src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+import App from './Component/App/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
